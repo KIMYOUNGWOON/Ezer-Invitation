31e4b629249d80c2e17a633c1feda9649bc3ae8e
kimyoungwoon <ywoon@gongdo.kr>
2024-12-20 17:54:57 +0900

    Open Graph 메타 태그 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,14 +15,6 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
-        <meta property="og:title" content="에제르 화실 회원전" />
-        <meta
-          property="og:description"
-          content="화실 에제르 회원들이 준비한 특별한 전시회가 열립니다."
-        />
-        <meta property="og:image" content="/images/thumbnail.png" />
-        <meta property="og:url" content="https://ezer-invitation.vercel.app" />
-        <meta property="og:type" content="website" />
         <Script
           type="text/javascript"
           src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&libraries=services,clusterer&autoload=false`}
